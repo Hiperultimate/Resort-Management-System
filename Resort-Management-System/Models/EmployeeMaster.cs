16f39a9fa7fbc59a54e7ab3da08b92369231f034
Hiperultimate <hiperultimatevegito@gmail.com>
2022-03-16 20:08:25 +0530

    Created login page login.

--- Resort-Management-System/Models/EmployeeMaster.cs
+++ Resort-Management-System/Models/EmployeeMaster.cs
@@ -11,7 +11,9 @@ namespace Resort_Management_System.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
     public partial class EmployeeMaster
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -21,12 +23,31 @@ namespace Resort_Management_System.Models
         }
     
         public int EmployeeID { get; set; }
+
+        [Required(ErrorMessage ="Enter employee name")]
         public string EmployeeName { get; set; }
+
+        [Required(ErrorMessage ="Enter employee email")]
+        [EmailAddress(ErrorMessage ="Enter a valid email address")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Enter a password")]
+        [MinLength(8, ErrorMessage ="Password should be 8 characters long")]
         public string Password { get; set; }
+
+        [Required]
         public string Address { get; set; }
+
+        [Required(ErrorMessage = "Enter a contact number")]
+        [RegularExpression("([0-9]+)", ErrorMessage ="Numbers only")]
+        [MaxLength(10,ErrorMessage ="Contact should be 10 characters long")]
         public long Contact { get; set; }
+
+        [ForeignKey("RolesMaster")]
         public int RoleID { get; set; }
+
+        [Required(ErrorMessage = "Enter a contact number")]
+        [RegularExpression("([0-9]+)",ErrorMessage ="Numbers only")]
         public long Salary { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
